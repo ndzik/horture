7cce175882cd9f158633eda3b8e65353deff562e
Norbert Dzikowski <norbert@perun.network>
2022-09-29 19:34:11 +0200

    Horture/Gif: Add missing TextureLength field to HortureGIF

--- src/Horture/Gif.hs
+++ src/Horture/Gif.hs
@@ -3,11 +3,14 @@ module Horture.Gif (HortureGIF (..)) where
 import Codec.Picture.Gif
 import Graphics.Rendering.OpenGL
 
+-- TODO: Do I want to track the ShaderProgram here too? Would be redundant
+-- state.
 data HortureGIF = HortureGIF
   { _gifFullPath :: !FilePath,
     _gifName :: !String,
     _gifTextureUnit :: !TextureUnit,
     _gifTextureObject :: !TextureObject,
+    _gifNumOfImgs :: !Int,
     _gifDelay :: !GifDelay
   }
   deriving (Show)
