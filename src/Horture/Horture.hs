866a3f53799347bb86e640bc255479156f8a1411
Norbert Dzikowski <norbert@perun.network>
2022-10-02 17:13:10 +0200

    Horture: Make datatype a MTL wrapper

--- src/Horture/Horture.hs
+++ src/Horture/Horture.hs
@@ -1,4 +1,7 @@
-module Horture.Horture (Horture, runHorture) where
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+module Horture.Horture (Horture (..), runHorture) where
 
 import Control.Monad.Except
 import Control.Monad.Reader
@@ -6,7 +9,28 @@ import Control.Monad.State
 import Horture.Error
 import Horture.State
 
-type Horture a = ExceptT HortureError (StateT HortureState (ReaderT HortureStatic IO)) a
+newtype Horture l a = Horture
+  { unHorture :: ExceptT HortureError (StateT HortureState (ReaderT HortureStatic IO)) a
+  }
+  deriving (Functor, Applicative, Monad, MonadIO)
 
-runHorture :: HortureState -> HortureStatic -> Horture a -> IO (Either HortureError a)
-runHorture ss rs = flip runReaderT rs . flip evalStateT ss . runExceptT
+instance MonadState HortureState (Horture l) where
+  get = Horture get
+  {-# INLINEABLE get #-}
+  put s = Horture $ put s
+  {-# INLINEABLE put #-}
+
+instance MonadReader HortureStatic (Horture l) where
+  ask = Horture ask
+  {-# INLINEABLE ask #-}
+  local mod (Horture h) = Horture $ local mod h
+  {-# INLINEABLE local #-}
+
+instance MonadError HortureError (Horture l) where
+  throwError err = Horture $ throwError err
+  {-# INLINEABLE throwError #-}
+  catchError (Horture act) h = Horture $ catchError act (unHorture . h)
+  {-# INLINEABLE catchError #-}
+
+runHorture :: HortureState -> HortureStatic -> Horture l a -> IO (Either HortureError a)
+runHorture ss rs = flip runReaderT rs . flip evalStateT ss . runExceptT . unHorture
