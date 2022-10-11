645c4857535df082e5432fd32db22b64ed9c0fa6
Norbert Dzikowski <norbert@perun.network>
2022-10-11 16:54:54 +0200

    Twitch/Rest: Define TokenApi for AppTokenRequests

--- src/Twitch/Rest.hs
+++ src/Twitch/Rest.hs
@@ -1,10 +1,34 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+
 module Twitch.Rest
   ( module Authentication,
     module Authorization,
     module Token,
+    twitchTokenClient,
+    TwitchTokenClient (..),
   )
 where
 
+import Data.Proxy
+import Servant.API
+import Servant.Client
 import Twitch.Rest.Authentication as Authentication
 import Twitch.Rest.Authorization as Authorization
 import Twitch.Rest.Token as Token
+
+newtype TwitchTokenClient = TwitchTokenClient
+  { getAppAccessToken :: ClientCredentialRequest -> ClientM ClientCredentialResponse
+  }
+
+type TokenApi = GetAppAccessToken
+
+type GetAppAccessToken = "token"
+  :> ReqBody '[FormUrlEncoded] ClientCredentialRequest :> Post '[JSON] ClientCredentialResponse
+
+twitchTokenClient :: TwitchTokenClient
+twitchTokenClient =
+  let _getAppAccessToken = client (Proxy @TokenApi)
+   in TwitchTokenClient
+        { getAppAccessToken = _getAppAccessToken
+        }
