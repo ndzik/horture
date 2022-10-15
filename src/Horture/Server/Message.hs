7e2aa81f6f63216d063e5fe154d69bd34cb04b5d
Norbert Dzikowski <norbert@perun.network>
2022-10-15 16:18:06 +0200

    Horture/Server/Message: Add message type for server origin

--- src/Horture/Server/Message.hs
+++ src/Horture/Server/Message.hs
@@ -1,11 +1,14 @@
-module Horture.Server.Message (HortureClientMessage (..)) where
+module Horture.Server.Message (HortureClientMessage (..),
+  HortureServerMessage(..)) where
 
 import Data.Aeson
 import Data.Aeson.TH
 import Data.Maybe (fromMaybe)
 import Data.Text (Text)
 import Network.WebSockets
+import qualified Twitch.EventSub.Event as Twitch
 
+-- | HortureClientMessage are all messages pushed to the server by client.
 data HortureClientMessage
   = HortureAuthorization
       { _userAccessToken :: !Text
@@ -21,4 +24,16 @@ instance WebSocketsData HortureClientMessage where
   fromLazyByteString bs = fromMaybe HortureGarbage (decode bs)
   toLazyByteString = encode
 
+-- | HortureServerMessage are all messages pushed by the server to clients.
+data HortureServerMessage
+  = HortureEventSub !Twitch.Event
+  | HortureServerGarbage
+
+instance WebSocketsData HortureServerMessage where
+  fromDataMessage (Network.WebSockets.Text bs _) = fromMaybe HortureServerGarbage (decode bs)
+  fromDataMessage (Network.WebSockets.Binary bs) = fromMaybe HortureServerGarbage (decode bs)
+  fromLazyByteString bs = fromMaybe HortureServerGarbage (decode bs)
+  toLazyByteString = encode
+
 $(deriveJSON defaultOptions ''HortureClientMessage)
+$(deriveJSON defaultOptions ''HortureServerMessage)
