cd7a4755e29825bc642a495771a687da5c3519ba
Norbert Dzikowski <norbert@perun.network>
2022-10-23 02:48:50 +0200

    Horture/CommandCenter: Add ability to refresh EventSource effects

--- src/Horture/CommandCenter/State.hs
+++ src/Horture/CommandCenter/State.hs
@@ -3,22 +3,27 @@
 module Horture.CommandCenter.State (CommandCenterState (..)) where
 
 import Brick.BChan
+import Control.Concurrent (ThreadId)
 import Control.Concurrent.Chan.Synchronous
 import Data.Default
+import qualified Data.Map.Strict as Map
 import Data.Text (Text)
 import Graphics.X11
 import Horture.CommandCenter.Event
+import Horture.Effect
 import Horture.Event
+import Horture.EventSource.Controller
 import Horture.Loader.Asset
-import Control.Concurrent (ThreadId)
 
 data CommandCenterState = CCState
   { _ccEventChan :: !(Maybe (Chan Event)),
     _brickEventChan :: !(Maybe (BChan CommandCenterEvent)),
     _ccCapturedWin :: !(Maybe (String, Window)),
+    _ccControllerChans :: !(Maybe (Chan EventControllerInput, Chan EventControllerResponse)),
     _ccLog :: ![Text],
     _ccGifs :: ![FilePath],
     _ccPreloadedGifs :: ![(FilePath, Asset)],
+    _ccRegisteredEffects :: !(Map.Map Text (Text, Effect)),
     _ccTIDsToClean :: ![ThreadId],
     -- | Timeout in microseconds for events to be generated. Only works in
     -- DEBUG mode.
@@ -31,6 +36,8 @@ instance Default CommandCenterState where
       { _ccEventChan = Nothing,
         _brickEventChan = Nothing,
         _ccCapturedWin = Nothing,
+        _ccControllerChans = Nothing,
+        _ccRegisteredEffects = Map.empty,
         _ccLog = [],
         _ccGifs = [],
         _ccPreloadedGifs = [],
