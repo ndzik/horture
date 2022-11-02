cd013b2d9f37b74ec68d8fa8b56b8276da5af0c6
Norbert Dzikowski <norbert@perun.network>
2022-11-03 00:31:30 +0100

    Horture/Initializer: Make initialization parameterized on backend

---
+++ src/horture/Horture/Initializer.hs
@@ -0,0 +1,7 @@
+module Horture.Initializer (
+  module I,
+  module E,
+  ) where
+
+import Horture.Initializer.Initializer as I
+import Horture.Initializer.Environment as E
---
+++ src/horture/Horture/Initializer/Initializer.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Horture.Initializer.Initializer
+  ( HortureInitializer (..),
+    runHortureInitializer,
+  )
+where
+
+import Colog
+import Control.Lens
+import Control.Monad.Except
+import Control.Monad.Reader
+import Horture.Error
+import Horture.Horture
+import Horture.Initializer.Environment
+import Horture.Logging
+
+runHortureInitializer :: HortureInitializerEnvironment -> HortureInitializer l hdl a -> IO (Either HortureError a)
+runHortureInitializer env = runExceptT . flip runReaderT env . unHortureInitializer
+
+newtype HortureInitializer (l :: LoggingTarget) hdl a = HortureInitializer
+  { unHortureInitializer :: ReaderT HortureInitializerEnvironment (ExceptT HortureError IO) a
+  }
+  deriving
+    ( Functor,
+      Applicative,
+      Monad,
+      MonadIO,
+      MonadError HortureError,
+      MonadReader HortureInitializerEnvironment
+    )
+
+instance HortureLogger (HortureInitializer 'Channel hdl) where
+  logEvent ev = asks (Just . (^. logChan)) >>= (\la -> withColog Info la (logEvent' ev)) . logChannel
+  logInfo msg = asks (Just . (^. logChan)) >>= (\la -> withColog Info la msg) . logChannel
+  logError msg = asks (Just . (^. logChan)) >>= (\la -> withColog Error la msg) . logChannel
+  logWarn msg = asks (Just . (^. logChan)) >>= (\la -> withColog Warning la msg) . logChannel
+
+instance HortureLogger (HortureInitializer 'NoLog hdl) where
+  logEvent _ = return ()
+  logInfo _ = return ()
+  logError _ = return ()
+  logWarn _ = return ()
