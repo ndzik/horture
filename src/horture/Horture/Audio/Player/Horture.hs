dda6e2e5b68a3dabdbd562b077ea94de2ed79711
Norbert Dzikowski <norbert@perun.network>
2023-02-01 03:30:00 +0100

    Horture/Audio/Player: Load all audio samples in init step

--- src/horture/Horture/Audio/Player/Horture.hs
+++ src/horture/Horture/Audio/Player/Horture.hs
@@ -1,4 +1,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE TupleSections #-}
 
 module Horture.Audio.Player.Horture where
 
@@ -8,12 +11,24 @@ import Control.Monad.Except
 import Control.Lens
 import Horture.Audio.Player.Player
 import Horture.Audio.Player.Protea
+import Horture.Audio.Player.Effects
 import Horture.Horture
+import qualified Data.Map.Strict as Map
+import Sound.ProteaAudio.SDL as Protea (Sample, sampleFromFile)
 import Horture.Error
 import Horture.State
+import Horture.Logging
 
-instance AudioPlayer (Horture hdl l) where
-  initAudio = initProteaAudio
+instance (HortureLogger (Horture hdl l)) => AudioPlayer (Horture hdl l) where
+  initAudio = do
+    res <- initProteaAudio
+    unless res $ throwError AudioSinkUnavailableErr
+    env <- asks (^. audioEnv)
+    let files = Map.toList $ staticSoundFiles env
+    soundSamplesFiles <- mapM (\(n, fp) -> (n,) <$> sampleFromFile' fp 0.6) files
+    soundSamplesGenerated <- mapM (\(n, pcm) -> (n,) <$> generateSampleFromPCM pcm) [(FlashbangPeep, flashbangPeep)]
+    modify $ \s -> s & audioState %~ \as -> as { staticSounds = Map.fromList $ soundSamplesFiles ++ soundSamplesGenerated }
+    return res
   deinitAudio = deinitProteaAudio
   clearAudio = clearProteaAudio
   playAudio a = do
@@ -24,3 +39,6 @@ instance AudioPlayer (Horture hdl l) where
       Left _ -> throwError AudioSinkUnavailableErr
       Right _ -> pure ()
     modify (\s -> s & audioState .~ as')
+
+sampleFromFile' :: FilePath -> Float -> Horture l hdl Sample
+sampleFromFile' fp = liftIO . sampleFromFile fp
